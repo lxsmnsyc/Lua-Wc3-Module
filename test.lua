623001211d7722e3b03d3631faeafa0dd0386be0
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-30 17:49:52 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,13 +1,24 @@
 
 require "Lua-Wc3-Module"
-module.create("A")(function ()
-  return 100
+
+module.create("A", function() return 100 end)
+module.create("B", function() return 200 end)
+module.create("add", function ()
+  return function (a, b)
+    return a + b
+  end
 end)
-module.create("B")(function ()
+module.create("print", function ()
   return print
 end)
-module.create("C", {"A", "B"})(function (imports)
-  return imports.B(imports.A)
+module.create("addAB", function (require)
+  local A = require("A")
+  local B = require("B")
+  local add = require("add")
+  return add(A, B)
 end)
-module.init()
+module.create("init", function (require)
+  require("print")(require("addAB"))
+end)
 
+module.load("init")
