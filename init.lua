2b18496a1c0f5bf9c2861977c2093d91dde0b42e
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-30 17:49:46 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,57 +1,25 @@
-local cachedModule = {}
-local modules = {}
 local moduleLoaded = {}
-local moduleParents = {}
+local cachedModule = {}
 local moduleBody = {}
-
-local function createModule(name, dependency)
-  modules[name] = true
-  local parents = {}
-  
-  if (type(dependency) == "table") then
-    for _, v in ipairs(dependency) do
-      if (v ~= name) then
-        parents[#parents + 1] = v
-      end
-    end
-  end
-
-  moduleLoaded[name] = false
-  moduleParents[name] = parents
-  return function (body)
-    moduleBody[name] = body
-  end
-end
+local origin = ""
 
 local function loadModule(name)
-  if (not moduleLoaded[name]) then
-    local result
-    local body = moduleBody[name]
-    local parents = moduleParents[name]
-    moduleLoaded[name] = true
-    if (#parents > 0) then
-      local imports = {}
-      for _, v in ipairs(parents) do
-        imports[v] = loadModule(v)
-      end
-      result = body(imports)
-    else
-      result = body()
-    end
-    cachedModule[name] = result
-    return result
+  if((not moduleLoaded[name]) and origin ~= name) then
+    local parent = origin
+    origin = name
+    cachedModule[name] = moduleBody[name](loadModule)
+    origin = parent
   end
   return cachedModule[name]
 end
 
-local function loadModules()
-  for k, _ in pairs(modules) do
-    loadModule(k)
-  end
+local function moduleCreate(name, body)
+  moduleBody[name] = body
+  moduleLoaded[name] = false
+  cachedModule[name] = nil
 end
 
 module = {
-  create = createModule,
-  load = loadModule,
-  init = loadModules,
+  create = moduleCreate,
+  load = loadModule
 }
