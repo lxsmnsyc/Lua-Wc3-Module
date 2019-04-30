42722d6ad8571fa244f7af4a4abdb0dbd24e4e69
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-30 18:02:53 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,24 +1,33 @@
 
 require "Lua-Wc3-Module"
 
-module.create("A", function() return 100 end)
-module.create("B", function() return 200 end)
-module.create("add", function ()
-  return function (a, b)
-    return a + b
+module.create("foo", function(require)
+  Foo = {}
+  Bar = Bar or require "bar"
+  Foo.name = 'foo'
+
+  function Foo:getName()
+      return Foo.name .. Bar.name
   end
+
+  return Foo
 end)
-module.create("print", function ()
-  return print
+module.create("bar", function(require)
+  Bar = {}
+  Foo = Foo or require "foo"
+  Bar.name = 'bar'
+
+  function Bar:getName()
+      return Bar.name .. Foo.name
+  end
+
+  return Bar
 end)
-module.create("addAB", function (require)
-  local A = require("A")
-  local B = require("B")
-  local add = require("add")
-  return add(A, B)
-end)
-module.create("init", function (require)
-  require("print")(require("addAB"))
+module.create("main", function (require)
+  Foo = Foo or require "foo"
+  Bar = Bar or require "bar"
+  print (Foo.getName())
+  print (Bar.getName())
 end)
 
-module.load("init")
+module.load("main")
